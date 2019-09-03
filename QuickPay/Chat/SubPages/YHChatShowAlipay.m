f0f364139bca759403cbc731ed5c508e3a5ae12e
Robert <Robert@local>
2019-09-03 21:59:30 +0700

    tmp

--- QuickPay/Chat/SubPages/YHChatShowAlipay.m
+++ QuickPay/Chat/SubPages/YHChatShowAlipay.m
@@ -35,43 +35,48 @@
 #import "MASConstraintMaker.h"
 
 
-
 @interface YHChatShowAlipay () {
 
 }
 
-@property(nonatomic, strong) NSMutableArray *dataArray;
-@property(nonatomic, strong) NSMutableArray *layouts;
+@property(nonatomic, strong) UIImageView *imgPayBg;
+/*----------------------------账号----------------------*/
 
-@property (nonatomic,strong) UIImageView *imgPayBg;
+@property(nonatomic, strong) UIImageView *imgPayBgType1;  // 不变
+@property(nonatomic, strong) UIImageView *imgPaySubline;  // 不变
 
-@property (nonatomic,strong) UIImageView *imgPayQrcodeBg;
-@property (nonatomic,strong) UIImageView *imgPayIcon;
-@property (nonatomic,strong) UIImageView *imgPaySubline;
-@property (nonatomic,strong) UILabel *lbTitle;
-@property (nonatomic,strong) UILabel *lbPayTips;
+@property(nonatomic, strong) UIImageView *imgIconPayType;
+@property(nonatomic, strong) UILabel *lbTitlePayType;
+@property(nonatomic, strong) UILabel *lbPayTipsPayType;
 
 // 账号-"内容"-复制
-@property (nonatomic,strong) UILabel *lbPayAccountsT;
-@property (nonatomic,strong) UILabel *lbPayAccountsContens;
-@property (nonatomic,strong) UILabel *lbPayAccountsCopy;
+@property(nonatomic, strong) UILabel *lbPayAccountsT;
+@property(nonatomic, strong) UILabel *lbPayAccountsContens;
+@property(nonatomic, strong) UILabel *lbPayAccountsCopy;
 
 // 姓-"内容"-复制
-@property (nonatomic,strong) UILabel *lbPayFirstNameT;
-@property (nonatomic,strong) UILabel *lbPayFirstNameContens;
-@property (nonatomic,strong) UILabel *lbPayFirstNameCopy;
+@property(nonatomic, strong) UILabel *lbPayFirstNameT;
+@property(nonatomic, strong) UILabel *lbPayFirstNameContens;
+@property(nonatomic, strong) UILabel *lbPayFirstNameCopy;
 
 // 名-"内容"-复制
-@property (nonatomic,strong) UILabel *lbPayLastNameT;
-@property (nonatomic,strong) UILabel *lbPayLastNameContens;
-@property (nonatomic,strong) UILabel *lbPayLastNameCopy;
+@property(nonatomic, strong) UILabel *lbPayLastNameT;
+@property(nonatomic, strong) UILabel *lbPayLastNameContens;
+@property(nonatomic, strong) UILabel *lbPayLastNameCopy;
 
 // 姓名-"内容"-复制
-@property (nonatomic,strong) UILabel *lbPayFullNameT;
-@property (nonatomic,strong) UILabel *lbPayFullNameContens;
-@property (nonatomic,strong) UILabel *lbPayFullNameCopy;
+@property(nonatomic, strong) UILabel *lbPayFullNameT;
+@property(nonatomic, strong) UILabel *lbPayFullNameContens;
+@property(nonatomic, strong) UILabel *lbPayFullNameCopy;
 
+/* -------------------------二维码---------------------ß */
 
+@property(nonatomic, strong) UIImageView *imgQcodePayBgType;  // 不变
+@property(nonatomic, strong) UIImageView *imgQcodePaySubline;  // 不变
+
+@property(nonatomic, strong) UIImageView *imgQcodeIconPayType;
+@property(nonatomic, strong) UILabel *lbQcodeTitlePayType;
+@property(nonatomic, strong) UILabel *lbQcodePayTipsPayType;
 
 @end
 
@@ -100,74 +105,65 @@
     // [self.navigationController.navigationBar setShadowImage:[self.tabBar lineImageWithColor:[UIColor colorWithHexString:@"#fb9966"]]];
 
     self.title = @"收款";
+    [self setupNavigationBar];
     [self setupTypeAccountsUI];
-    [self layoutUI];
+    [self layoutTypeAccountsUI];
+
+    [self setupTypeQrcodeUI];
+    [self layoutTypeQrcodeUI];
+
 }
 
 
-- (NSMutableArray *)dataArray {
-    if (!_dataArray) {
-        _dataArray = [NSMutableArray new];
-    }
-    return _dataArray;
-}
-
-- (NSMutableArray *)layouts {
-    if (!_layouts) {
-        _layouts = [NSMutableArray new];
-    }
-    return _layouts;
-}
-
-
-
-- (void)setupTypeAccountsUI {
-
+- (void)setupNavigationBar {
     self.navigationController.navigationBar.translucent = NO;
     self.navigationController.navigationBar.backgroundColor = RGB16(0x6ba5f7);
 
-    self.view.backgroundColor = RGB16(0x6ba5f7); //RGBCOLOR(239, 236, 236);
+}
 
+- (void)setupTypeAccountsUI {
+
+    self.view.backgroundColor = RGB16(0x6ba5f7); //RGBCOLOR(239, 236, 236);
 //    _imgPayBg= [UIImageView new];
 //    _imgPayBg.image = [UIImage imageNamed:@"alipay_bg"];;
 //    [self.view addSubview:_imgPayBg];
 
     // 二维码背景
-    _imgPayQrcodeBg = [UIImageView new];
+    _imgPayBgType1 = [UIImageView new];
     UIImage *imgBg = [UIImage imageNamed:@"pay_show_bg"];
     imgBg = [imgBg resizableImageWithCapInsets:UIEdgeInsetsMake(30, 15, 30, 30) resizingMode:UIImageResizingModeStretch];
-    _imgPayQrcodeBg.image = imgBg;
-    [self.view addSubview:_imgPayQrcodeBg];
+    _imgPayBgType1.image = imgBg;
+    [self.view addSubview:_imgPayBgType1];
 
 
-    _imgPayIcon = [UIImageView new];
+    _imgIconPayType = [UIImageView new];
     UIImage *imgIcon = [UIImage imageNamed:@"icon_alipay"];
-    _imgPayIcon.image = imgIcon;
-    [self.imgPayQrcodeBg addSubview:_imgPayIcon];
+    _imgIconPayType.image = imgIcon;
+    [self.imgPayBgType1 addSubview:_imgIconPayType];
 
 
     _imgPaySubline = [UIImageView new];
     _imgPaySubline.image = [UIImage imageNamed:@"pay_subline"];
     _imgPaySubline.alpha = 30;
-    [self.imgPayQrcodeBg addSubview:_imgPaySubline];
+    [self.imgPayBgType1 addSubview:_imgPaySubline];
 
 
     // 提示背景
-    _lbTitle = [UILabel new];
-    _lbTitle.font = [UIFont systemFontOfSize:16.0];
-    _lbTitle.numberOfLines = 1;
-    _lbTitle.lineBreakMode = NSLineBreakByTruncatingMiddle;
-    _lbTitle.textColor = [UIColor blackColor];
-    _lbTitle.text = @"支付宝账号转帐";
-    [self.imgPayQrcodeBg addSubview:_lbTitle];
+    _lbTitlePayType = [UILabel new];
+    _lbTitlePayType.font = [UIFont systemFontOfSize:16.0];
+    _lbTitlePayType.numberOfLines = 1;
+    _lbTitlePayType.lineBreakMode = NSLineBreakByTruncatingMiddle;
+    _lbTitlePayType.textColor = [UIColor blackColor];
+    _lbTitlePayType.text = @"支付宝账号转帐";
+    [self.imgPayBgType1 addSubview:_lbTitlePayType];
 
-    _lbPayTips = [UILabel new];
-    _lbPayTips.font = [UIFont systemFontOfSize:16.0];
-    _lbPayTips.numberOfLines = 1;
-    _lbPayTips.lineBreakMode = NSLineBreakByTruncatingMiddle;
-    _lbPayTips.textColor = [UIColor blackColor];
-    _lbPayTips.text = @"复制支付宝账号，支付宝[转账]付钱";
-    [self.imgPayQrcodeBg addSubview:_lbPayTips];
+    _lbPayTipsPayType = [UILabel new];
+    _lbPayTipsPayType.font = [UIFont systemFontOfSize:16.0];
+    _lbPayTipsPayType.numberOfLines = 1;
+    _lbPayTipsPayType.lineBreakMode = NSLineBreakByTruncatingMiddle;
+    _lbPayTipsPayType.textColor = [UIColor blackColor];
+    _lbPayTipsPayType.text = @"复制支付宝账号，支付宝[转账]付钱";
+    [self.imgPayBgType1 addSubview:_lbPayTipsPayType];
 
     // 支付宝账号一行
     _lbPayAccountsT = [UILabel new];
@@ -176,7 +172,7 @@
     _lbPayAccountsT.lineBreakMode = NSLineBreakByTruncatingMiddle;
     _lbPayAccountsT.textColor = [UIColor blackColor];
     _lbPayAccountsT.text = @"支付宝账号";
-    [self.imgPayQrcodeBg addSubview:_lbPayAccountsT];
+    [self.imgPayBgType1 addSubview:_lbPayAccountsT];
 
     _lbPayAccountsContens = [UILabel new]; //
     _lbPayAccountsContens.font = [UIFont systemFontOfSize:14.0];
@@ -185,7 +181,7 @@
     _lbPayAccountsContens.textAlignment = NSTextAlignmentCenter;
     _lbPayAccountsContens.textColor = [UIColor blackColor];
     _lbPayAccountsContens.text = @"1234567890123456@qq.com";
-    [self.imgPayQrcodeBg addSubview:_lbPayAccountsContens];
+    [self.imgPayBgType1 addSubview:_lbPayAccountsContens];
 
     _lbPayAccountsCopy = [UILabel new];
     _lbPayAccountsCopy.font = [UIFont systemFontOfSize:16.0];
@@ -193,7 +189,7 @@
     _lbPayAccountsCopy.lineBreakMode = NSLineBreakByTruncatingMiddle;
     _lbPayAccountsCopy.textColor = [UIColor blueColor];
     _lbPayAccountsCopy.text = @"复制";
-    [self.imgPayQrcodeBg addSubview:_lbPayAccountsCopy];
+    [self.imgPayBgType1 addSubview:_lbPayAccountsCopy];
 
     // 支付宝名
     _lbPayFirstNameT = [UILabel new];
@@ -202,7 +198,7 @@
     _lbPayFirstNameT.lineBreakMode = NSLineBreakByTruncatingMiddle;
     _lbPayFirstNameT.textColor = [UIColor blackColor];
     _lbPayFirstNameT.text = @"支付宝姓";
-    [self.imgPayQrcodeBg addSubview:_lbPayFirstNameT];
+    [self.imgPayBgType1 addSubview:_lbPayFirstNameT];
 
     _lbPayFirstNameContens = [UILabel new]; //
     _lbPayFirstNameContens.font = [UIFont systemFontOfSize:16.0];
@@ -211,7 +207,7 @@
     _lbPayFirstNameContens.textAlignment = NSTextAlignmentCenter;
     _lbPayFirstNameContens.textColor = [UIColor blackColor];
     _lbPayFirstNameContens.text = @"邓";
-    [self.imgPayQrcodeBg addSubview:_lbPayFirstNameContens];
+    [self.imgPayBgType1 addSubview:_lbPayFirstNameContens];
 
     _lbPayFirstNameCopy = [UILabel new];
     _lbPayFirstNameCopy.font = [UIFont systemFontOfSize:16.0];
@@ -219,7 +215,7 @@
     _lbPayFirstNameCopy.lineBreakMode = NSLineBreakByTruncatingMiddle;
     _lbPayFirstNameCopy.textColor = [UIColor blueColor];
     _lbPayFirstNameCopy.text = @"复制";
-    [self.imgPayQrcodeBg addSubview:_lbPayFirstNameCopy];
+    [self.imgPayBgType1 addSubview:_lbPayFirstNameCopy];
 
     // 支付宝姓
     _lbPayLastNameT = [UILabel new];
@@ -228,7 +224,7 @@
     _lbPayLastNameT.lineBreakMode = NSLineBreakByTruncatingMiddle;
     _lbPayLastNameT.textColor = [UIColor blackColor];
     _lbPayLastNameT.text = @"支付宝名";
-    [self.imgPayQrcodeBg addSubview:_lbPayLastNameT];
+    [self.imgPayBgType1 addSubview:_lbPayLastNameT];
 
     _lbPayLastNameContens = [UILabel new]; //
     _lbPayLastNameContens.font = [UIFont systemFontOfSize:16.0];
@@ -237,7 +233,7 @@
     _lbPayLastNameContens.textAlignment = NSTextAlignmentCenter;
     _lbPayLastNameContens.textColor = [UIColor blackColor];
     _lbPayLastNameContens.text = @"超";
-    [self.imgPayQrcodeBg addSubview:_lbPayLastNameContens];
+    [self.imgPayBgType1 addSubview:_lbPayLastNameContens];
 
     _lbPayLastNameCopy = [UILabel new];
     _lbPayLastNameCopy.font = [UIFont systemFontOfSize:16.0];
@@ -245,7 +241,7 @@
     _lbPayLastNameCopy.lineBreakMode = NSLineBreakByTruncatingMiddle;
     _lbPayLastNameCopy.textColor = [UIColor blueColor];
     _lbPayLastNameCopy.text = @"复制";
-    [self.imgPayQrcodeBg addSubview:_lbPayLastNameCopy];
+    [self.imgPayBgType1 addSubview:_lbPayLastNameCopy];
 
     // 支付宝全名
     _lbPayFullNameT = [UILabel new];
@@ -254,7 +250,7 @@
     _lbPayFullNameT.lineBreakMode = NSLineBreakByTruncatingMiddle;
     _lbPayFullNameT.textColor = [UIColor blackColor];
     _lbPayFullNameT.text = @"支付宝姓名";
-    [self.imgPayQrcodeBg addSubview:_lbPayFullNameT];
+    [self.imgPayBgType1 addSubview:_lbPayFullNameT];
 
     _lbPayFullNameContens = [UILabel new]; //
     _lbPayFullNameContens.font = [UIFont systemFontOfSize:16.0];
@@ -263,7 +259,7 @@
     _lbPayFullNameContens.textAlignment = NSTextAlignmentCenter;
     _lbPayFullNameContens.textColor = [UIColor blackColor];
     _lbPayFullNameContens.text = @"邓超";
-    [self.imgPayQrcodeBg addSubview:_lbPayFullNameContens];
+    [self.imgPayBgType1 addSubview:_lbPayFullNameContens];
 
     _lbPayFullNameCopy = [UILabel new];
     _lbPayFullNameCopy.font = [UIFont systemFontOfSize:16.0];
@@ -271,17 +267,18 @@
     _lbPayFullNameCopy.lineBreakMode = NSLineBreakByTruncatingMiddle;
     _lbPayFullNameCopy.textColor = [UIColor blueColor];
     _lbPayFullNameCopy.text = @"复制";
-    [self.imgPayQrcodeBg addSubview:_lbPayFullNameCopy];
+    [self.imgPayBgType1 addSubview:_lbPayFullNameCopy];
+
 }
 
-- (void)layoutUI{
+- (void)layoutTypeAccountsUI {
     WeakSelf
 //    [_imgPayBg mas_makeConstraints:^(MASConstraintMaker *make) {
 //        // 添加左、上边距约束
 //        make.left.and.right.and.bottom.and.top.mas_equalTo(0);
 //    }];
 
-    [_imgPayQrcodeBg mas_makeConstraints:^(MASConstraintMaker *make) {
+    [_imgPayBgType1 mas_makeConstraints:^(MASConstraintMaker *make) {
         // 添加左、上边距约束
         make.left.and.top.mas_equalTo(10);
         // 添加右边距约束
@@ -289,44 +286,44 @@
         make.height.mas_equalTo(268);
     }];
 
-    [_imgPayIcon mas_makeConstraints:^(MASConstraintMaker *make) {
+    [_imgIconPayType mas_makeConstraints:^(MASConstraintMaker *make) {
         make.width.height.mas_equalTo(28);
         make.top.mas_equalTo(5);
-        make.right.equalTo(weakSelf.lbTitle.mas_left).offset(-5);
+        make.right.equalTo(weakSelf.lbTitlePayType.mas_left).offset(-5);
 
     }];
 
 
-    [_lbTitle mas_makeConstraints:^(MASConstraintMaker *make) {
+    [_lbTitlePayType mas_makeConstraints:^(MASConstraintMaker *make) {
         // 添加左、上边距约束
-        make.centerX.mas_equalTo(_imgPayQrcodeBg).offset(3);
+        make.centerX.mas_equalTo(_imgPayBgType1).offset(3);
         // 添加右边距约束
         make.top.mas_equalTo(10);
     }];
 
     [_imgPaySubline mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.topMargin.equalTo(_imgPayIcon).offset(32);
+        make.topMargin.equalTo(_imgIconPayType).offset(32);
         make.leftMargin.and.rightMargin.mas_equalTo(1);
     }];
 
-    [_lbPayTips mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.mas_equalTo(_imgPayQrcodeBg);
+    [_lbPayTipsPayType mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(_imgPayBgType1);
         make.top.mas_equalTo(_imgPaySubline).offset(20);
     }];
 
     // 支付宝账号一行
     [_lbPayAccountsT mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(_lbPayTips).offset(40);
+        make.top.mas_equalTo(_lbPayTipsPayType).offset(40);
         make.left.mas_equalTo(10);
     }];
 
     [_lbPayAccountsContens mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(_lbPayTips).offset(40);
+        make.top.mas_equalTo(_lbPayTipsPayType).offset(40);
         make.right.mas_equalTo(_lbPayAccountsCopy).offset(-40);
     }];
 
     [_lbPayAccountsCopy mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(_lbPayTips).offset(40);
+        make.top.mas_equalTo(_lbPayTipsPayType).offset(40);
         make.right.mas_equalTo(-10);
     }];
 
@@ -380,6 +377,87 @@
     }];
 }
 
+
+- (void)setupTypeQrcodeUI {
+
+    self.view.backgroundColor = RGB16(0x6ba5f7); //RGBCOLOR(239, 236, 236);
+
+    // 二维码背景
+    _imgQcodePayBgType = [UIImageView new];
+    UIImage *imgBg = [UIImage imageNamed:@"pay_show_bg"];
+    imgBg = [imgBg resizableImageWithCapInsets:UIEdgeInsetsMake(30, 15, 30, 30) resizingMode:UIImageResizingModeStretch];
+    _imgQcodePayBgType.image = imgBg;
+    [self.view addSubview:_imgQcodePayBgType];
+
+
+    _imgQcodeIconPayType = [UIImageView new];
+    UIImage *imgIcon = [UIImage imageNamed:@"icon_alipay"];
+    _imgQcodeIconPayType.image = imgIcon;
+    [self.imgQcodePayBgType addSubview:_imgQcodeIconPayType];
+
+
+    _imgQcodePaySubline = [UIImageView new];
+    _imgQcodePaySubline.image = [UIImage imageNamed:@"pay_subline"];
+    _imgQcodePaySubline.alpha = 30;
+    [self.imgQcodePayBgType addSubview:_imgQcodePaySubline];
+
+
+    // 提示背景
+    _lbQcodeTitlePayType = [UILabel new];
+    _lbQcodeTitlePayType.font = [UIFont systemFontOfSize:16.0];
+    _lbQcodeTitlePayType.numberOfLines = 1;
+    _lbQcodeTitlePayType.lineBreakMode = NSLineBreakByTruncatingMiddle;
+    _lbQcodeTitlePayType.textColor = [UIColor blackColor];
+    _lbQcodeTitlePayType.text = @"支付宝账号转帐";
+    [self.imgQcodePayBgType addSubview:_lbQcodeTitlePayType];
+
+    _lbQcodePayTipsPayType = [UILabel new];
+    _lbQcodePayTipsPayType.font = [UIFont systemFontOfSize:16.0];
+    _lbQcodePayTipsPayType.numberOfLines = 1;
+    _lbQcodePayTipsPayType.lineBreakMode = NSLineBreakByTruncatingMiddle;
+    _lbQcodePayTipsPayType.textColor = [UIColor blackColor];
+    _lbQcodePayTipsPayType.text = @"复制支付宝账号，支付宝[转账]付钱";
+    [self.imgQcodePayBgType addSubview:_lbQcodePayTipsPayType];
+
+
+}
+
+- (void)layoutTypeQrcodeUI {
+    [self.imgPayBgType1 setHidden:YES];
+
+    WeakSelf
+    [_imgQcodePayBgType mas_makeConstraints:^(MASConstraintMaker *make) {
+        // 添加左、上边距约束
+        make.left.and.top.mas_equalTo(10);
+        // 添加右边距约束
+        make.right.mas_equalTo(-10);
+        make.height.mas_equalTo(268);
+    }];
+
+    [_imgQcodeIconPayType mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.height.mas_equalTo(28);
+        make.top.mas_equalTo(5);
+        make.right.equalTo(weakSelf.lbTitlePayType.mas_left).offset(-5);
+    }];
+
+
+    [_lbQcodeTitlePayType mas_makeConstraints:^(MASConstraintMaker *make) {
+        // 添加左、上边距约束
+        make.centerX.mas_equalTo(_imgQcodePayBgType).offset(3);
+        // 添加右边距约束
+        make.top.mas_equalTo(10);
+    }];
+
+    [_imgQcodePaySubline mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.topMargin.equalTo(_imgQcodeIconPayType).offset(32);
+        make.leftMargin.and.rightMargin.mas_equalTo(1);
+    }];
+
+    [_lbQcodePayTipsPayType mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(_imgQcodePayBgType);
+        make.top.mas_equalTo(_imgQcodePaySubline).offset(20);
+    }];
+}
 
 #pragma mark - Action
 
